6d302a3b69dd2a3f4433d03d450ccfaa1a911598
virtual-frame-7789 <mttwright@gmail.com>
2019-09-21 20:00:28 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,4 +11,13 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    else
+      1
+    end
+  end
+end
